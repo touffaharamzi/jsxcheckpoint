05533d01d207424d32fe4694c55462938c6b3174
touffaharamzi <ramzitouffaha98@gmail.com>
2022-04-09 13:42:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import image from "./cat.jpg"
+import './style.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+<div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+    <h1 className="title red">Ramzi Touffaha</h1>
+    <br />
+    <img src={image} alt ='myImage' />
+    <br />
+    <img src="/public.jpg" alt="public" />
+  </div>
+  <video width={320} height={240} controls="">
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+</div>
   );
 }
 
